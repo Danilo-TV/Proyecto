4a0ee8af4547359ed33b47a9cc3563e53ca34ea4
angelrendon807 <angelrendon807@gmail.com>
2025-10-13 23:59:23 +0000

    frontend de la landing page

--- administracion_portafolio/settings.py
+++ administracion_portafolio/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # Añadido para permitir peticiones del frontend
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
